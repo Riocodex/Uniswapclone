6da112eddd0ba8091b17796fadfc9f66da67c46b
Riocodex <onwukachibike@gmail.com>
2023-06-21 19:18:25 +0100

    updated

--- src/AlphaRouterService.js
+++ src/AlphaRouterService.js
@@ -4,3 +4,22 @@ const { ethers, BigNumber } = require('ethers')
 
 const JSBI = require('jsbi')
 const ERC20ABI = require('./abi.json')
+
+const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
+const INFURA_URL_TESTNET =  process.env.REACT_APP_INFURA_URL_TESTNET 
+
+chanId = 111519
+
+const Web3Provider = new ethers.providers.JsonRpcProvider(REACT_APP_INFURA_URL_TESTNET)
+const router = new AlphaRouter({chainId : chainId, provider : Web3Provider})
+
+
+const name0 = 'Wrapped Ether'
+const symbol0 = 'WETH'
+const decimals0 = 18
+const address0 = '0xc778417e063141139fce010982780140aa0cd5ab'
+
+const name1 = 'Uniswap Token'
+const symbol1 = 'UNI'
+const decimals1 = 18
+const address1 = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
