594a12cd5c85593c6009af1b7da5057b9375a218
Riocodex <onwukachibike@gmail.com>
2023-06-21 20:15:13 +0100

    updated

--- src/AlphaRouterService.js
+++ src/AlphaRouterService.js
@@ -1,3 +1,5 @@
+import { Currency } from '@uniswap/sdk'
+
 const { AlphaRouter } = require('@uniswap/smart-order-router')
 const { Token, CurrencyAmount, TradeType, Percent } = require('@uniswap/sdk-core')
 const { ethers, BigNumber } = require('ethers')
@@ -17,9 +19,43 @@ const router = new AlphaRouter({chainId : chainId, provider : Web3Provider})
 const name0 = 'Wrapped Ether'
 const symbol0 = 'WETH'
 const decimals0 = 18
-const address0 = '0xc778417e063141139fce010982780140aa0cd5ab'
+const address0 = '0xc778417e063141139fce010982780140aa0cd5ab'//needs to be changed to sepolia address
 
 const name1 = 'Uniswap Token'
 const symbol1 = 'UNI'
 const decimals1 = 18
-const address1 = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+const address1 = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'//this is from ropsten..needs to be changed to sepolia
+
+
+const WETH = new Token(chainId, address0, decimals0, symbol0, name0)
+const UNI = new Token(chainId, address1, decimals1, symbol1, name1)
+
+export const getWethContract= () => new ethers.Contract(address0, ERC20ABI, web3Provider)
+export const getUniContract= () => new ethers.Contract(address1, ERC20ABI, web3Provider)
+
+export const getPrice = async (inputAmount, slippageAmount, deadline, walletAddress) =>{
+    const percentSlippage = new Percent(slippageAmount,100)
+    const wei = ethers.utils.parseUnits(inputAmount.toString(), decimals0)
+    const CurrencyAmount = CurrencyAmount.fromRawAmount(WETH,JSBI.BigInt(wei))
+
+    const route = await router.route(
+        CurrencyAmount,
+        UNI,
+        TradeType.EXACT_INPUT,
+        {
+            recipient: walletAddress,
+            slippageTolerance: percentSlippage,
+            deadline: deadline,
+        }
+    )
+
+    const transaction = {
+        data: route.methodParameters.calldata,
+        to: V3_SWAP_ROUTER_ADDRESS,
+        value: BigNumber.from(route.methodParameters.value),
+        from: walletAddress,
+        gasPrice: BigNumber.from(route.gasPriceWei),
+        gasLimit: ethers.utils.hexlify(1000000)
+    }
+    
+}
